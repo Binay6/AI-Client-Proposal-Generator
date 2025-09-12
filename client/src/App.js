25ff16ef61aea31baff3eb1d7751b1e37e7e9327
Binay Maity <Binay3662@gmail.com>
2025-09-12 23:22:59 +0530

    Initial commit with all project files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Binay Maity</h1>
+      <h2>Full Stack Developer</h2>
+    </div>
+  );
+}
+
+export default App;
